590946e2576a9f8cb61acc7de4d9739e91818786
Rudy Dellomas <dther@dther.xyz>
2021-10-20 19:53:19 +1000

    Centralised priority colourising statements

--- tashkcolor.sh
+++ tashkcolor.sh
@@ -9,7 +9,8 @@
 #   m4 won't work because the todotxt spec is strict and m4 macro names
 #   can't contain parens
 
-bblack='[1;30m'
+# bold/bright
+bblack='[1;30m' # Grey on most terminals
 bred='[1;31m'
 bgreen='[1;32m'
 byellow='[1;33m'
@@ -17,36 +18,44 @@ bblue='[1;34m'
 bmagenta='[1;35m'
 bcyan='[1;36m'
 bwhite='[1;37m'
+bold='[1m' # no colour
 
 green='[0;32m'
 magenta='[0;35m'
 white='[0;37m'
+black='[0;30m' # Completely invisible on some terminals.
 
 strike='[9m'
 
 clr='[0m'
 sed "
+	# Find priorities
+	/^(A)/s/^\(.*\)$/<<<A>>>\1/
+	/^(B)/s/^\(.*\)$/<<<B>>>\1/
+	/^(C)/s/^\(.*\)$/<<<C>>>\1/
+	/^(D)/s/^\(.*\)$/<<<D>>>\1/
+
+	# find completed
+	/^x/s/^\(.*\)$/<<<x>>>\1/
+
 	# projects + contexts
-	# <<<ATTR>>> will be replaced with appropriate terminal attributes
 	s/ \(+[[:graph:]]*\)/ $magenta\1<<<ATTR>>>/g
 	s/ \(@[[:graph:]]*\)/ $green\1<<<ATTR>>>/g
 
-	# reset attributes
-	/^(A)/s/<<<ATTR>>>/${bred}/g
-	/^(B)/s/<<<ATTR>>>/${byellow}/g
-	/^(C)/s/<<<ATTR>>>/${bblue}/g
-	/^(D)/s/<<<ATTR>>>/${bmagenta}/g
-	/^x /s/<<<ATTR>>>/${bblack}${strike}/g
+	# change <<<ATTR>>> to correct default line attributes
+	/<<<A>>>/s/<<<ATTR>>>/<<<A>>>/g
+	/<<<B>>>/s/<<<ATTR>>>/<<<B>>>/g
+	/<<<C>>>/s/<<<ATTR>>>/<<<C>>>/g
+	/<<<D>>>/s/<<<ATTR>>>/<<<D>>>/g
+	/<<<x>>>/s/<<<ATTR>>>/<<<x>>>/g
 	s/<<<ATTR>>>/$clr/g; # default
 
-	# complete colouring priorities
-	/^(A)/s/^\(.*\)$/$bred\1/
-	/^(B)/s/^\(.*\)$/$byellow\1/
-	/^(C)/s/^\(.*\)$/$bblue\1/
-	/^(D)/s/^\(.*\)$/$bmagenta\1/
-
-	# strike completed
-	/^x /s/^\(.*\)$/$bblack$strike\1/
+	# change <<<foo>>> tags to an appropriate escape sequence
+	s/<<<A>>>/$bred/g
+	s/<<<B>>>/$byellow/g
+	s/<<<C>>>/$bblue/g
+	s/<<<D>>>/$bold/g
+	s/<<<x>>>/$bblack$strike/g
 
 	# clear on newline
 	s/$/$clr/
