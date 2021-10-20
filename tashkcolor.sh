9319bbb4ef859320525245de61b441e2bc1fc7b9
Rudy Dellomas <dther@dther.xyz>
2021-10-20 13:15:27 +1000

    Grey out completed tasks in tashkcolor (for terminals that can't strike)

--- tashkcolor.sh
+++ tashkcolor.sh
@@ -3,6 +3,7 @@
 # obv. assumes that these escape codes work
 # NOTE: for "done" tasks to work properly,
 # this has to be done BEFORE piping to nl...
+bblack='[1;30m'
 bred='[1;31m'
 bgreen='[1;32m'
 byellow='[1;33m'
@@ -12,6 +13,7 @@ bcyan='[1;36m'
 bwhite='[1;37m'
 
 magenta='[0;35m'
+white='[0;37m'
 
 strike='[9m'
 
@@ -22,4 +24,4 @@ sed "\
 	/(C)/s/^\(.*\)$/$bblue\1$clr/
 	/(D)/s/^\(.*\)$/$bmagenta\1$clr/
 	s/\(+[[:graph:]]*\)/$magenta\1$clr/
-	/^x /s/^\(.*\)$/$strike\1$clr/"
+	/^x /s/^\(.*\)$/$bblack$strike\1$clr/"
